4717d7eca7fb05041bd5f1ff9caa532b2e5bde96
Leonardo <leon.alonzo.leo@gmail.com>
2022-07-29 15:51:00 -0500

    componentes 60-70

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,36 @@
+import 'screens.dart';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+   
+    const HomeScreen({Key? key}) : super(key: key);
+    
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: const Text('Componentes en Flutter'),
+                elevation: 0,
+            ),
+            body: 
+                ListView.separated(
+                    itemBuilder:(context, index) => ListTile(
+                        leading: const Icon(Icons.access_time_outlined),
+                        title: const Text('Nombre de ruta'),
+                        onTap: () {
+
+                            //final route = MaterialPageRoute(builder: (context) => const Listview1Screen());
+                            //Navigator.push( context, route );
+                            //Navigator.pushReplacement(context, route); //Destruye el screen anterior, por ejemplo para despues del loguin
+
+                            Navigator.pushNamed(context, 'card');
+
+                        },
+                    ), 
+                    separatorBuilder: ( _ , __ ) => const Divider(), 
+                    itemCount: 100
+                )
+        );
+    }
+}
