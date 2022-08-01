e9ef3259f418fb9ecd3eb87b0f1c6dba5e3750b2
Leonardo <leon.alonzo.leo@gmail.com>
2022-08-01 17:13:18 -0500

    Argumentos a los Widgets

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -18,7 +18,12 @@ class CardScreen extends StatelessWidget {
                 children: const [
                     CustomCardType1(),
                     SizedBox(height: 10),
-                    CustomCardType2(),
+                    CustomCardType2(imageUrl: 'https://photographylife.com/wp-content/uploads/2017/01/What-is-landscape-photography.jpg', name:'Imagen 1'),
+                    SizedBox(height: 20),
+                    CustomCardType2(imageUrl: 'https://media-exp1.licdn.com/dms/image/C4D1BAQFsdjpzrtQWUA/company-background_10000/0/1519796755846?e=2147483647&v=beta&t=tA2WEp7x9015ge1Px-zpkFFUueL-uskh1SlXHTBCXrM', name:'Imagen 2'),
+                    SizedBox(height: 20),
+                    CustomCardType2(imageUrl: 'https://wallpaperaccess.com/full/112722.jpg', name:'Imagen 3'),
+                    SizedBox(height: 40),
                 ],
             )
         );
--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -2,7 +2,12 @@ import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
-    const CustomCardType2({Key? key}) : super(key: key);
+    const CustomCardType2({Key? key, required this.imageUrl, this.name}) : super(key: key);
+
+    final String imageUrl;
+    final String? name;
+
+    //'https://photographylife.com/wp-content/uploads/2017/01/What-is-landscape-photography.jpg'
 
     @override
     Widget build(BuildContext context) {
@@ -15,20 +20,21 @@ class CustomCardType2 extends StatelessWidget {
             shadowColor: AppTheme.primary.withOpacity(0.5),
             child: Column(
                 children: [
-                    const FadeInImage(
-                        image: NetworkImage('https://photographylife.com/wp-content/uploads/2017/01/What-is-landscape-photography.jpg'), 
-                        placeholder: AssetImage('assets/jar-loading.gif'),
+                    FadeInImage(
+                        image: NetworkImage(imageUrl), 
+                        placeholder: const  AssetImage('assets/jar-loading.gif'),
                         width: double.infinity,
                         height: 230,
                         fit: BoxFit.cover,
-                        fadeInDuration: Duration(milliseconds: 300),
+                        fadeInDuration: const Duration(milliseconds: 300),
                     ),
-
-                    Container(
-                        alignment: AlignmentDirectional.centerEnd,
-                        padding: const EdgeInsets.only(right:20, top:10, bottom: 10),
-                        child:  const Text('Un hermoso paisaje')
-                    )
+                    
+                    if( name != null)
+                        Container(
+                            alignment: AlignmentDirectional.centerEnd,
+                            padding: const EdgeInsets.only(right:20, top:10, bottom: 10),
+                            child: Text( name ?? '' )
+                        )
                 ]
             ),
         );
