ad4f525c4642791c038373e8790641f50dcf5f72
Leonardo <leon.alonzo.leo@gmail.com>
2022-08-02 12:16:24 -0500

    Mostrar una alerta

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,6 +3,35 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
    
     const AlertScreen({Key? key}) : super(key: key);
+
+
+    void displayDialog(BuildContext context){
+        showDialog(
+            barrierDismissible: false,
+            context: context, 
+            builder: (context) {
+                return AlertDialog(
+                    elevation: 5,
+                    title: const Text('Titulo'),
+                    shape: RoundedRectangleBorder( borderRadius: BorderRadiusDirectional.circular(15)),
+                    content: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: const [
+                            Text('Este es el contenido de la alerta'),
+                            SizedBox(height: 10),
+                            FlutterLogo(size: 100)
+                        ]
+                    ),
+                    actions: [
+                        TextButton( 
+                            onPressed: () => Navigator.pop(context), 
+                            child: const Text('Cancelar')
+                        )
+                    ],
+                );
+            }
+        );
+    }
     
     @override
     Widget build(BuildContext context) {
@@ -14,13 +43,11 @@ class AlertScreen extends StatelessWidget {
                         shape: const StadiumBorder(),
                         elevation: 15
                     ), */
+                    onPressed: () => displayDialog(context),
                     child: const Padding(
                       padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
                       child: Text('Mostrar alerta', style: TextStyle( fontSize: 16 )),
-                    ),
-                    onPressed: (){
-
-                    }, 
+                    )
                 )
             ),
             floatingActionButton: FloatingActionButton(
