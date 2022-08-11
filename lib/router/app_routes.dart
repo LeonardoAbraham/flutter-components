e20f67c42b5be9c98a68ec035fd3fffa41a6a6c8
Leonardo <leon.alonzo.leo@gmail.com>
2022-08-11 10:02:27 -0500

    Listview Builder

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -19,6 +19,7 @@ class AppRoutes{
         MenuOption(route: 'animated', icon: Icons.play_circle_outline_rounded, name: 'Animated Container', screen: const AnimatedScreen()),
         MenuOption(route: 'inputs', icon: Icons.input_rounded, name: 'Text Inputs', screen: const InputsScreen()),
         MenuOption(route: 'slider', icon: Icons.slow_motion_video_rounded, name: 'Slider && Checks', screen: const SliderScreen()),
+        MenuOption(route: 'listviewbuilder', icon: Icons.build_circle_outlined, name: 'InfiniteScroll & Pull to refresh', screen: const ListViewBuilderScreen()),
     ];
 
     static Map<String, Widget Function(BuildContext)> getAppRoutes(){
